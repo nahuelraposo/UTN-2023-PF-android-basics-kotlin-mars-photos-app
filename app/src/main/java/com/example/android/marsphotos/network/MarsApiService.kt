b95bb8f8f2d34fc815b2de4e1954ecabb7341966
Nahuel Raposo <nahuelraposo@gmail.com>
2023-07-16 17:12:32 -0300

    App with mini-module 3 ended

--- app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.android.marsphotos.network
 
 import com.squareup.moshi.Moshi
@@ -6,18 +22,19 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
-private const val BASE_URL =
-    "https://android-kotlin-fun-mars-server.appspot.com"
+private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com/"
 
 /**
- * Build the Moshi object with Kotlin adapter factory that Retrofit will be using.
+ * Build the Moshi object that Retrofit will be using, making sure to add the Kotlin adapter for
+ * full Kotlin compatibility.
  */
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
     .build()
 
 /**
- * The Retrofit object with the Moshi converter.
+ * Use the Retrofit builder to build a retrofit object using a Moshi converter with our Moshi
+ * object.
  */
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(MoshiConverterFactory.create(moshi))
@@ -34,7 +51,7 @@ interface MarsApiService {
      * HTTP method
      */
     @GET("photos")
-    suspend fun getPhotos() : List<MarsPhoto>
+    suspend fun getPhotos(): List<MarsPhoto>
 }
 
 /**
@@ -43,4 +60,3 @@ interface MarsApiService {
 object MarsApi {
     val retrofitService: MarsApiService by lazy { retrofit.create(MarsApiService::class.java) }
 }
-
